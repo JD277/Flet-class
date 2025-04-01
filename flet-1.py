ea4abaaf8dedc9422e0b517fc9ec32b4a12e850f
jdam <jdam5000@gmail.com>
2025-04-01 13:00:13 -0400

    Class 1 added for understand the differents way that flet works

---
+++ flet-1.py
@@ -0,0 +1,22 @@
+# Impoting libraries
+import flet as ft
+
+def main(page:ft.Page):
+    """
+        Es la función principal del programa
+        
+        Args:
+            page: una pagina de flet
+    """
+    label = ft.Text("Escribe tu Nombre")
+    output_text = ft.Text()
+
+    name = ft.TextField()
+
+    def btn_click(e):
+        output_text.value = f"Hello, {name.value}!"
+        page.update()
+    my_btn = ft.ElevatedButton("Enviar",on_click=btn_click)
+    page.add(label,name,output_text,my_btn)
+
+ft.app(target=main,view=ft.WEB_BROWSER)
